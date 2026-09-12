5b8b0c139f783679eb7caf0b1076a2feac0e8f26
Thembisile Makhubu <makhubuts4@gmail.com>
2026-09-12 22:52:04 +0200

    Add test route for debugging

---
+++ app/test/page.tsx
@@ -0,0 +1,8 @@
+export default function TestPage() {
+  return (
+    <div style={{ padding: "50px", fontSize: "24px" }}>
+      <h1>Test page works!</h1>
+      <p>If you can see this, Vercel is serving your app correctly.</p>
+    </div>
+  );
+}
